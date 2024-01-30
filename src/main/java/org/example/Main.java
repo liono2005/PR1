cc428bf7f336d81a24778b428069c06d6753f5d8
rejme <git@github.com:liono2005/PR1.git>
2024-01-30 20:42:22 +0300

    ygfgd

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-         Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
          System.out.println("Hello, world");
          System.out.println("+++");
          System.out.println("!!!!");
