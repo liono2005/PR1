19997745abc26f440b1bc1f58eb842cab1b40eb3
rejme <git@github.com:liono2005/PR1.git>
2024-01-30 20:41:34 +0300

    fdreysdsfhsfth

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);
-        System.out.println("Hello, world");
-        System.out.println("+++");
-        System.out.println("!!!!");
-        System.out.println("??");
+         Scanner in = new Scanner(System.in);
+         System.out.println("Hello, world");
+         System.out.println("+++");
+         System.out.println("!!!!");
+         System.out.println("??");
     }
 }
